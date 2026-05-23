ceb099731b06f888ce032dc645ad82a18bff59fc
Ricardo T <rtorre79@email.asu.edu>
2026-05-22 18:00:25 -0700

    add playRound function to have logic to play a round..

--- javascript.js
+++ javascript.js
@@ -2,13 +2,13 @@ function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3) + 1;
     
     if (rand === 1) {
-        return "Computer: Rock!";
+        return "rock";
     }
     else if (rand === 2) {
-        return "Computer: Paper!";
+        return "paper";
     }
     else if (rand === 3) {
-        return "Computer: Scissors!";
+        return "scissors";
     }
 }
 
@@ -16,17 +16,48 @@ function getHumanChoice() {
     let choice = parseInt(prompt("Choose: 1 = Rock, 2 = Paper, 3 = Scissors"));
 
     if (choice === 1) {
-        return "Player: Rock!";
+        return "Rock";
     }
     else if (choice === 2) {
-        return "Player: Paper!";
+        return "Paper";
     }
     else if (choice === 3) {
-        return "Player: Scissors";
+        return "Scissors";
     }
 }
 
 let humanScore = 0;
 let computerScore = 0;
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
 
+    if (humanChoice === computerChoice)
+    {
+        return "Tie!";
+    }
 
+    /* 1 = Rock
+       2 = Paper
+       3 = Scissors
+    */
+    else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        return `Player wins! ${humanChoice} beats ${computerChoice}!`;
+    }
+    else {
+        computerScore++;
+        return `Computer Wins! ${computerChoice} beats ${humanChoice}`;
+    }
+    
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+console.log(`Human Score: ${humanScore}`);
+console.log(`Computer Score: ${computerScore}`);
