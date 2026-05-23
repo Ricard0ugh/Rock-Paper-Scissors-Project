24e5d8b5150f2ab4b48c2354c243b960a5c2447b
Ricardo T <rtorre79@email.asu.edu>
2026-05-22 17:14:22 -0700

    Add getComputerChoice function
    Replace the previous hello log with getComputerChoice(), which generates a random integer 1–3 and returns a corresponding 'Computer: Rock!/Paper!/Scissors!' string. The chosen option is then logged to the console to enable basic computer move generation for a Rock-Paper-Scissors game.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let rand = Math.floor(Math.random() * 3) + 1;
+    
+    if (rand === 1) {
+        return "Computer: Rock!";
+    }
+    else if (rand === 2) {
+        return "Computer: Paper!";
+    }
+    else if (rand === 3) {
+        return "Computer: Scissors!";
+    }
+}
+
+
+console.log(getComputerChoice());
